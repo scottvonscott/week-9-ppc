30b0461f33a5391b2146ecc37133a12fe27aa14a
hansenjl <jenn.leigh.hansen@gmail.com>
2020-07-09 14:08:14 -0700

    add routes and models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Party.create([
+    { name: 'Moroccan Christmas', date:'2019-12-16', budget:50, private:false },
+    { name: 'Goodbye Toby', date:'2020-7-20', budget:200, private:false },
+    { name: 'Nutcracker Christmas', date: '2020-12-15', budget:25, private:true}
+])
