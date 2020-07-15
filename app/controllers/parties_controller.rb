9a07240b1c427dbc935a51058f4e015435f4b56c
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2020-07-15 12:24:12 -0700

    Update parties_controller.rb

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -8,10 +8,21 @@ class PartiesController < ApplicationController
     end
 
     def create
-        byebug
+        @party = Party.new(party_params)
+        if @party.save 
+            redirect_to party_path(@party)
+        else
+            render :new
+        end
     end
 
     def show
         @party = Party.find(params[:id])
+    end
+    
+    private
+    
+    def party_params
+        params.require(:party).permit(:date, :budget, :name)
     end
 end
